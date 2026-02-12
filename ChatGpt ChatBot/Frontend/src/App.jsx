96f8508f2521d63a6ec87408ec424f93067026df
Md Arsalan Ali <officialmdarsalanali@gmail.com>
2026-02-12 21:43:56 +0530

    Add backend and frontend structure for chat application
    - Created backend services including conversation management, error handling, and chat generation using OpenAI API.
    - Implemented frontend with React and Vite, featuring chat layout, sidebar, and context management.
    - Added necessary configuration files and dependencies for both backend and frontend.
    - Established .gitignore files for both backend and frontend to exclude unnecessary files.

---
+++ ChatGpt ChatBot/Frontend/src/App.jsx
@@ -0,0 +1,27 @@
+
+import { BrowserRouter, Route, Routes} from "react-router-dom"
+import ChatLayout from "./layout/ChatLayout";
+import Chat from "./components/Chat";
+import { ChatProvider } from "./store/ChatContext";
+
+
+function App() {
+  return (
+    
+   <BrowserRouter>
+   <ChatProvider>
+  <div className="min-h-screen bg-gray-100">
+    <Routes>
+      <Route path="/" element={<ChatLayout />}>
+        <Route index element={<Chat />} />
+        <Route path="conversation/:id" element={<Chat />} />
+      </Route>
+    </Routes>
+  </div>
+  </ChatProvider>
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
